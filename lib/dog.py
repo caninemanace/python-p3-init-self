8911d95ed3f60e3371753eee573b647290148cc1
Eddie Njoroge <eddie.njoroge2306@gmail.com>
2025-05-12 21:58:27 +0300

    assignment complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name , breed = "Mutt"):
+        self.breed = breed
+        self.name = name
+fido = Dog("Fido")
+fido.breed = "Golden Retriever"
+print(fido.breed)
+print(fido.name)
